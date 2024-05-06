19f09b39d84cc14b9601481a5a5ac7e6a4ac5fa4
Paul Schroeder <paul.schroeder@nuc>
2024-05-06 22:15:12 +0200

    chore: generate

--- config/external_name.go
+++ config/external_name.go
@@ -24,7 +24,7 @@ var ExternalNameConfigs = map[string]config.ExternalName{
 	"cloudflare_account":                                config.IdentifierFromProvider,
 	"cloudflare_api_shield":                             config.IdentifierFromProvider,
 	"cloudflare_api_token":                              config.IdentifierFromProvider,
-	"cloudflare_argo_tunnel":                            config.IdentifierFromProvider,
+	"cloudflare_tunnel":                                 config.IdentifierFromProvider,
 	"cloudflare_argo":                                   config.IdentifierFromProvider,
 	"cloudflare_authenticated_origin_pulls_certificate": config.IdentifierFromProvider,
 	"cloudflare_authenticated_origin_pulls":             config.IdentifierFromProvider,
